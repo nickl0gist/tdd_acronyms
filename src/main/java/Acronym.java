51118bfece30751e8f01bb306a989a44879bbc12
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 21:25:28 +0100

    Update Acronym.java

--- src/main/java/Acronym.java
+++ src/main/java/Acronym.java
@@ -1,18 +1,1 @@
-class Acronym {
 
-    private final String acronym;
-
-    Acronym(String phrase) {
-        StringBuilder sbuf = new StringBuilder();
-        String[] chunks = phrase.split("[\\s\\-]");
-        for(String chunk: chunks){
-            sbuf.append(chunk.charAt(0));
-        }
-        this.acronym = sbuf.toString().toUpperCase();
-    }
-
-    String get() {
-        return this.acronym;
-    }
-
-}
