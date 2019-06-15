2bcdec51d6fbae7789b2f6edc234e0689dac5211
nickl0gist <nicklogistick@mail.ru>
2019-06-15 13:15:49 +0200

    Acronym

--- src/main/java/Acronym.java
+++ src/main/java/Acronym.java
@@ -1,1 +1,17 @@
+public class Acronym {
 
+    String phrase;
+
+    public Acronym(String phrase) {
+        this.phrase = phrase;
+    }
+
+    public String get() {
+        String[] arr = phrase.split("[\\s-]");
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            str.append(Character.toString(arr[i].charAt(0)).toUpperCase());
+        }
+        return str.toString();
+    }
+}
